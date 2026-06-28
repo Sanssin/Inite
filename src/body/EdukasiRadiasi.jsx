04b3a6c2425aab0eb57d80eabc6534a356714527
Sanssin <nurihsanudin13@gmail.com>
2026-06-28 20:40:09 +0700

    chore: remove unused useEffect import in EdukasiRadiasi

--- src/body/EdukasiRadiasi.jsx
+++ src/body/EdukasiRadiasi.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
